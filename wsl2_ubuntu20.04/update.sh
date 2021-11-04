ff8297ade1c8d6281722edcb4743243c8fb005d5
joseesco24 <josed.escobar@outlook.com>
2021-11-04 15:37:39 -0500

    separacion de scripts para ubuntu nativo y wsl2

---
+++ wsl2_ubuntu20.04/update.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -e
+
+echo -e
+sudo -k
+
+scripts_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+home=$HOME
+user=$USER
+
+sudo -v
+echo -e
+
+echo -e "iniciando actualizaciones"
+
+echo -e
+echo -e "actualizando versiones de repositorios y paquetes"
+echo -e
+
+sudo apt-get update -y --fix-missing
+
+echo -e
+echo -e "instalando repositorios y paquetes actualizados"
+echo -e
+
+sudo apt-get --fix-broken install -y
+sudo apt-get upgrade -y --fix-missing
+
+echo -e
+echo -e "comprobando y actualizando version del kernel"
+echo -e
+
+sudo apt-get dist-upgrade -y --fix-missing
+
+echo -e
+echo -e "comprobando instalaciones nuevas"
+echo -e
+
+sudo apt-get check -y
+
+echo -e
+echo -e "reparando paquetes y dependencias dañadas"
+echo -e
+
+sudo apt --fix-broken -y install
+
+echo -e
+echo -e "eliminando paquetes y repositorios innecesarios"
+echo -e
+
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo apt remove -y
+sudo apt clean -y
+
+echo -e
+echo -e "actualizaciones finalizadas"
+echo -e
