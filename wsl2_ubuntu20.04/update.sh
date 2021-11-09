655e63260d220cf1cb3fc1e21e364232f1ee1fe5
joseesco24 <josed.escobar@outlook.com>
2021-11-09 09:49:14 -0500

    correcciones menores en los scripts de wsl2

--- wsl2_ubuntu20.04/update.sh
+++ wsl2_ubuntu20.04/update.sh
@@ -10,44 +10,32 @@ remove_and_ask_password
 
 print_title "Iniciando Actualizaciones"
 
-print_title "1/8 - Actualizando Versiones De Repositorios Y Paquetes"
+print_title "1/6 - Actualizando Versiones De Repositorios Y Paquetes"
 
 sudo apt-get update -y --fix-missing
 
-print_title "2/8 - Instalando Repositorios Y Paquetes Actualizados"
+print_title "2/6 - Instalando Repositorios Y Paquetes Actualizados"
 
 sudo apt-get --fix-broken install -y
 sudo apt-get upgrade -y --fix-missing
 
-print_title "3/8 - Comprobando Y Actualizando Version Del Kernel"
+print_title "3/6 - Comprobando Y Actualizando Version Del Kernel"
 
 sudo apt-get dist-upgrade -y --fix-missing
 
-print_title "4/8 - Comprobando Instalaciones Nuevas"
+print_title "4/6 - Comprobando Instalaciones Nuevas"
 
 sudo apt-get check -y
 
-print_title "5/8 - Reparando Paquetes Y Dependencias Dañadas"
+print_title "5/6 - Reparando Paquetes Y Dependencias Dañadas"
 
 sudo apt --fix-broken -y install
 
-print_title "6/8 - Eliminando Paquetes Y Repositorios Innecesarios"
+print_title "6/6 - Eliminando Paquetes Y Repositorios Innecesarios"
 
 sudo apt autoremove -y
 sudo apt autoclean -y
 sudo apt remove -y
 sudo apt clean -y
-
-print_title "7/8 - Verificando Permisos De Los Scripts Personalizados"
-
-sudo find $scripts_path -type f -exec chmod 777 {} \;
-
-print_text "verificación de permisos finalizada"
-
-print_title "8/8 - Verificando Permisos De Los Directorios Y Subdirectorios De Home"
-
-sudo find $home/ -type d -exec chmod 755 {} \;
-
-print_text "verificación de permisos finalizada"
 
 print_title "Actualizaciones Finalizadas"
