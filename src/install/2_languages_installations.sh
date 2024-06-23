7b854daec326ae0b13b436ccc072eb8a27c1c4db
joseesco24 <josed.escobar@outlook.com>
2024-06-22 23:52:27 -0500

    build: versions updates and bun installation added

--- src/install/2_languages_installations.sh
+++ src/install/2_languages_installations.sh
@@ -10,7 +10,7 @@ print_title "Starting Installations"
 sudo $scripts_path/../sidecar/update.sh
 
 quiet_update
-print_title "01/05 - Installing Python 3.12.3"
+print_title "01/06 - Installing Python 3.12.4"
 
 if command -v python3.12.0 &> /dev/null; then
 	print_text "python3.12 is already installed, no further changes needed"
@@ -21,9 +21,9 @@ else
 	cd python
 	sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 	quiet_update
-	wget https://www.python.org/ftp/python/3.12.3/Python-3.12.3.tar.xz
-	tar -xvf Python-3.12.3.tar.xz
-	cd Python-3.12.3
+	wget https://www.python.org/ftp/python/3.12.4/Python-3.12.4.tar.xz
+	tar -xvf Python-3.12.4.tar.xz
+	cd Python-3.12.4
 	sudo ./configure --enable-optimizations
 	sudo make altinstall
 	cd ..
@@ -32,7 +32,7 @@ else
 fi
 
 quiet_update
-print_title "02/05 - Installing Poetry Latest"
+print_title "02/06 - Installing Poetry Latest"
 
 if command -v poetry &> /dev/null; then
 	print_text "poetry is already installed, no further changes needed"
@@ -43,7 +43,7 @@ else
 fi
 
 quiet_update
-print_title "03/05 - Installing Rust Latest"
+print_title "03/06 - Installing Rust Latest"
 
 if command -v rustup &> /dev/null; then
 	print_text "rust is already installed, no further changes needed"
@@ -56,7 +56,7 @@ else
 fi
 
 quiet_update
-print_title "04/05 - Installing Nodejs 22.2.0"
+print_title "04/06 - Installing Nodejs 22.2.0"
 
 if command -v node &> /dev/null; then
 	print_text "nodejs is already installed, no further changes needed"
@@ -65,15 +65,15 @@ else
 	quiet_update
 	mkdir node
 	cd node
-	wget https://nodejs.org/dist/v22.2.0/node-v22.2.0-linux-x64.tar.xz
-	tar -xvf node-v22.2.0-linux-x64.tar.xz
-	sudo cp -r node-v22.2.0-linux-x64/{bin,include,lib,share} /usr/
+	wget https://nodejs.org/dist/v22.3.0/node-v22.3.0-linux-x64.tar.xz
+	tar -xvf node-v22.3.0-linux-x64.tar.xz
+	sudo cp -r node-v22.3.0-linux-x64/{bin,include,lib,share} /usr/
 	cd ..
 	rm -r node
 fi
 
 quiet_update
-print_title "05/05 - Installing Go 1.22.3"
+print_title "05/06 - Installing Go 1.22.3"
 
 if command -v go &> /dev/null; then
 	print_text "go is already installed, no further changes needed"
@@ -82,11 +82,22 @@ else
 	quiet_update
 	mkdir golang
 	cd golang
-	wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+	wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
 	rm -rf /usr/local/go
-	sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
 	cd ..
 	rm -r golang
+fi
+
+quiet_update
+print_title "03/06 - Installing Bun 1.1.16"
+
+if command -v bun &> /dev/null; then
+	print_text "bun is already installed, no further changes needed"
+else
+	print_text "bun is not installed, installing"
+	quiet_update
+	curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.16"
 fi
 
 print_title "Installations Completed"
