b33569565dc1abdd5969668ea1d172c49f0de227
joseesco24 <josed.escobar@outlook.com>
2025-01-22 16:39:58 -0500

    feat: langeage versions updates

--- src/install/2_languages_installations.sh
+++ src/install/2_languages_installations.sh
@@ -10,20 +10,20 @@ print_title "Starting Installations"
 sudo $scripts_path/../sidecar/update.sh
 
 quiet_update
-print_title "01/06 - Installing Python 3.12.4"
+print_title "01/06 - Installing Python 3.13.1 Latest"
 
-if command -v python3.12.0 &> /dev/null; then
-	print_text "python3.12 is already installed, no further changes needed"
+if command -v python3.13.0 &> /dev/null; then
+	print_text "python3.13 is already installed, no further changes needed"
 else
-	print_text "python3.12 is not installed, installing"
+	print_text "python3.13 is not installed, installing"
 	quiet_update
 	mkdir python
 	cd python
 	sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 	quiet_update
-	wget https://www.python.org/ftp/python/3.12.4/Python-3.12.4.tar.xz
-	tar -xvf Python-3.12.4.tar.xz
-	cd Python-3.12.4
+	wget https://www.python.org/ftp/python/3.13.1/Python-3.13.1.tar.xz
+	tar -xvf Python-3.13.1.tar.xz
+	cd Python-3.13.1
 	sudo ./configure --enable-optimizations
 	sudo make altinstall
 	cd ..
@@ -39,7 +39,7 @@ if command -v poetry &> /dev/null; then
 else
 	print_text "poetry is not installed, installing"
 	quiet_update
-	curl -sSL https://install.python-poetry.org | python3.12 -
+	curl -sSL https://install.python-poetry.org | python3.13 -
 fi
 
 quiet_update
@@ -56,7 +56,7 @@ else
 fi
 
 quiet_update
-print_title "04/06 - Installing Nodejs 22.2.0"
+print_title "04/06 - Installing Nodejs 22.13.1 LTS"
 
 if command -v node &> /dev/null; then
 	print_text "nodejs is already installed, no further changes needed"
@@ -65,15 +65,15 @@ else
 	quiet_update
 	mkdir node
 	cd node
-	wget https://nodejs.org/dist/v22.3.0/node-v22.3.0-linux-x64.tar.xz
-	tar -xvf node-v22.3.0-linux-x64.tar.xz
-	sudo cp -r node-v22.3.0-linux-x64/{bin,include,lib,share} /usr/
+	wget https://nodejs.org/dist/v22.13.1/node-v22.13.1-linux-x64.tar.xz
+	tar -xvf node-v22.13.1-linux-x64.tar.xz
+	sudo cp -r node-v22.13.1-linux-x64/{bin,include,lib,share} /usr/
 	cd ..
 	rm -r node
 fi
 
 quiet_update
-print_title "05/06 - Installing Go 1.22.3"
+print_title "05/06 - Installing Go 1.23.5 Stable"
 
 if command -v go &> /dev/null; then
 	print_text "go is already installed, no further changes needed"
@@ -82,26 +82,26 @@ else
 	quiet_update
 	mkdir golang
 	cd golang
-	wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+	wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
 	rm -rf /usr/local/go
-	sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
 	cd ..
 	rm -r golang
 fi
 
 quiet_update
-print_title "03/06 - Installing Bun 1.1.16"
+print_title "03/06 - Installing Bun 1.2.0 Latest"
 
 if command -v bun &> /dev/null; then
 	print_text "bun is already installed, no further changes needed"
 else
 	print_text "bun is not installed, installing"
 	quiet_update
-	curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.16"
+	curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.0"
 fi
 
 quiet_update
-print_title "03/06 - Installing Java 21"
+print_title "03/06 - Installing Java 21 LTS"
 
 if command -v java &> /dev/null; then
 	print_text "java is already installed, no further changes needed"
--- src/install/3_languages_config.sh
+++ src/install/3_languages_config.sh
@@ -5,7 +5,7 @@ source $scripts_path/../sidecar/commons.sh
 
 print_title "Starting Languages Configuration"
 
-echo "export PATH=$HOME/.cargo/bin:$HOME/.local/bin:/usr/node-v20.8.0-linux-x64/bin:/usr/local/go/bin:$PATH" | tee -a ~/.bashrc
+echo "export PATH=$HOME/.cargo/bin:$HOME/.local/bin:/usr/node-v22.13.1-linux-x64/bin:/usr/local/go/bin:$PATH" | tee -a ~/.bashrc
 source ~/.bashrc
 
 print_title "Languages Configuration Completed"
